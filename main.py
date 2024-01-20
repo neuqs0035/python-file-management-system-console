a4a3fa51b4c86e6a999dde8e1f8c29f564d8f1f1
neuqs0035 <bhavyapadaliya179@gmail.com>
2024-01-20 22:01:16 +0530

    CREATE NEW DICTIONARY AT CURRENT DIRECTORY OR ANY OTHER CODE AND OPTION ADDED

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ while True:
     print("\n[ --- --- Main Menu --- --- ]")
 
     print("\n1 . Create New File")
+    print("2 . Create New Directory")
     print("0 . Exit")
 
     choice = input("\n_ : ")
+
+    at_current_dir = ""
 
     if choice.isdigit():
 
@@ -55,6 +58,52 @@ while True:
                 else:
 
                     print("\nirectory Not Found , Please Enter Valid Directory Path")
+
+        elif choice == 2:
+
+            print("\n--- Create New Directory ---")
+
+            at_current_dir = input("\nDo You Wanna Create New Directory At Current Directory ? (y / n) : ")
+
+            if at_current_dir.lower() == "y":
+
+                dir_name = input("\nEnter New Directory Name : ")
+
+                if os.path.exists(dir_name):
+
+                    print("\nSame Named Directory Already Exists ")
+
+                else:
+
+                    os.mkdir(dir_name)
+
+                    print("\nDirectory Created Successfully")
+
+            else:
+
+                root_location = input("\nEnter The Path Till Root Folder Where You Wanna Create A Directory : ")
+
+                if os.path.exists(root_location):
+
+                    dir_name = input("\nEnter Directory Name : ")
+
+                    final_path = os.path.join(root_location,dir_name)
+
+                    if os.path.exists(final_path): 
+
+                        print("\nSame Named Directory Already Exists")
+
+                    else:
+
+                        os.mkdir(final_path)
+
+                        print("\nDirectory Created Successfully ( full path ) " + final_path)
+
+                else:
+
+                    print("\nPlease Enter Valid Path")
+
+            
         elif choice == 0:
 
             print("\nSystem Exited .......")
